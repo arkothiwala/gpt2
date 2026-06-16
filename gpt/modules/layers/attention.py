5748aaded7aa7946a7a94fe8dce881b8a6fe5643
arkothiwala <ashutosh.kothiwala@gmail.com>
2026-06-16 13:05:00 +0530

    updated attention and transformer blocks to contain custom MHA implementation

--- gpt/modules/layers/attention.py
+++ gpt/modules/layers/attention.py
@@ -20,3 +20,35 @@ class Attention(torch.nn.Module):
         scaled_dot_product = dot_product / (self.d_attention ** 0.5) # (B, L_q, L_k)
         attention = torch.nn.softmax(scaled_dot_product, dim=-1) # (B, L_q, L_k)
         return attention@v # (B, L_v, d_attention)
+
+class CustomMultiHeadAttention(torch.nn.Module):
+    def __init__(self, embed_dim, num_heads, dropout=None, bias=True, batch_first=True):
+        super().__init__()
+        self.d_model = embed_dim
+        self.n_heads = num_heads
+        self.dropout = dropout if dropout else 0.0
+        self.d_attention = self.d_model//self.n_heads
+        self.in_proj = torch.nn.Linear(in_features=self.d_model, out_features=3*self.d_model, bias=bias)
+        self.in_proj_weight = self.in_proj.weight
+        self.out_proj = torch.nn.Linear(in_features=self.d_attention*self.n_heads, out_features=self.d_model, bias=bias)
+        if batch_first==False:
+            raise ValueError("this implementation only supports batch_first at the moment.")
+
+    def forward(self, x: torch.Tensor):
+        batch_size, seq_len, d_model = x.shape
+        x = self.in_proj(x) # (batch_size, seq_len, 3*d_model)
+        x = x.reshape(batch_size, seq_len, 3, self.n_heads, self.d_attention).permute(0, 3, 1, 4, 2).contiguous()
+        q,k,v = torch.unbind(x, dim=-1)
+        out = torch.nn.functional.scaled_dot_product_attention(
+            query=q,
+            key=k,
+            value=v,
+            dropout_p=self.dropout if self.training else 0.0, 
+            is_causal=True
+        ).permute(
+            0,2,1,3
+        ).contiguous().reshape(
+            batch_size, seq_len, d_model
+        )
+        out = self.out_proj(out)
+        return out
