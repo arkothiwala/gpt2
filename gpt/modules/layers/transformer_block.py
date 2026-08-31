79a6a95395242e7c0a9e37236688b49a55f65538
arkothiwala <ashutosh.kothiwala@gmail.com>
2026-08-31 06:56:50 +0000

    removed comments in the forward pass

--- gpt/modules/layers/transformer_block.py
+++ gpt/modules/layers/transformer_block.py
@@ -96,31 +96,46 @@ class TransformerBlock(torch.nn.Module):
         )
 
 
+    # def forward(self, x):
+    #     batch_size, seq_len, d_model = x.shape
+    #     x_layer_norm_mha = self.layer_norm_mha(x)
+    #     # self.logger.debug(f"x.shape = {x.shape} | x.device = {x.device} | x.dtype = {x.dtype}")
+    #     # self.logger.debug(f"x_layer_norm_mha.shape = {x_layer_norm_mha.shape} | x_layer_norm_mha.device = {x_layer_norm_mha.device} | x_layer_norm_mha.dtype = {x_layer_norm_mha.dtype}")
+    #     # MISTAKE - initially I had not provided separate query, key and values arguments. Also, I wasn't selecting first value
+    #     # This was because in my custom implementation of MHA, I wasn't taking three inputs in MHA.
+    #     # x_post_mha, attention = self.MHA(
+    #     #     query=x_layer_norm_mha, 
+    #     #     key=x_layer_norm_mha, 
+    #     #     value=x_layer_norm_mha, 
+    #     #     # attn_mask=torch.triu(torch.ones(seq_len, seq_len)*float("-inf"), diagonal=1, device=x.device).bool(), # not using is_causal=True because because using it along with need_weights=True is leading to silent or loud failures based on the pytorch version. Also, instead of float mask we are preparing boolean mask as it would consume much less memory, makes compute slightly faster and is recommended by PyTorch.
+    #     #     attn_mask=self.causal_mask[:seq_len, :seq_len], # using pre-registered buffer for causal mask. This would be more efficient as we are not creating the mask in every forward pass and also it would be on the same device as the model.
+    #     #     need_weights=False,
+    #     #     key_padding_mask=None, # given currently we are training models on full sequence length. This is additive mask. if key_padding_mask is boolean -> True is replaced with -inf and False is replaced with 0 in attention mask. if key_padding_mask is float -> values in key_padding_mask are directly added to attention mask. so we can directly provide key_padding_mask as attention mask for padding tokens.
+    #     #     is_causal=True # using built-in causal mask support in PyTorch which is more efficient and also works well with need_weights=True. This would automatically apply the causal mask to the attention scores before softmax.
+    #     # )
+    #     x_post_mha = self.MHA(
+    #         x=x_layer_norm_mha
+    #     )
+    #     # self.logger.debug(f"x_post_mha.dtype = {x_post_mha.dtype}")
+    #     x_post_mha = x + self.dropout_residual_mha(x_post_mha)
+    #     # self.logger.info(f"self.MHA.in_proj_weight.dtype = {self.MHA.in_proj_weight.dtype} | self.MHA.out_proj.weight.dtype = {self.MHA.out_proj.weight.dtype} | self.FFN.linear_projection.weight.dtype = {self.FFN.linear_projection.weight.dtype}")
+
+    #     x_layer_norm_ffn = self.layer_norm_ffn(x_post_mha)
+    #     x_post_ffn = x_post_mha + self.dropout_residual_ffn(self.FFN(x_layer_norm_ffn))
+    #     return x_post_ffn
+        
     def forward(self, x):
         batch_size, seq_len, d_model = x.shape
         x_layer_norm_mha = self.layer_norm_mha(x)
-        # self.logger.debug(f"x.shape = {x.shape} | x.device = {x.device} | x.dtype = {x.dtype}")
-        # self.logger.debug(f"x_layer_norm_mha.shape = {x_layer_norm_mha.shape} | x_layer_norm_mha.device = {x_layer_norm_mha.device} | x_layer_norm_mha.dtype = {x_layer_norm_mha.dtype}")
-        # MISTAKE - initially I had not provided separate query, key and values arguments. Also, I wasn't selecting first value
-        # This was because in my custom implementation of MHA, I wasn't taking three inputs in MHA.
-        # x_post_mha, attention = self.MHA(
-        #     query=x_layer_norm_mha, 
-        #     key=x_layer_norm_mha, 
-        #     value=x_layer_norm_mha, 
-        #     # attn_mask=torch.triu(torch.ones(seq_len, seq_len)*float("-inf"), diagonal=1, device=x.device).bool(), # not using is_causal=True because because using it along with need_weights=True is leading to silent or loud failures based on the pytorch version. Also, instead of float mask we are preparing boolean mask as it would consume much less memory, makes compute slightly faster and is recommended by PyTorch.
-        #     attn_mask=self.causal_mask[:seq_len, :seq_len], # using pre-registered buffer for causal mask. This would be more efficient as we are not creating the mask in every forward pass and also it would be on the same device as the model.
-        #     need_weights=False,
-        #     key_padding_mask=None, # given currently we are training models on full sequence length. This is additive mask. if key_padding_mask is boolean -> True is replaced with -inf and False is replaced with 0 in attention mask. if key_padding_mask is float -> values in key_padding_mask are directly added to attention mask. so we can directly provide key_padding_mask as attention mask for padding tokens.
-        #     is_causal=True # using built-in causal mask support in PyTorch which is more efficient and also works well with need_weights=True. This would automatically apply the causal mask to the attention scores before softmax.
-        # )
-        x_post_mha = self.MHA(
-            x=x_layer_norm_mha
-        )
-        # self.logger.debug(f"x_post_mha.dtype = {x_post_mha.dtype}")
+        x_post_mha = self.MHA(x=x_layer_norm_mha)
         x_post_mha = x + self.dropout_residual_mha(x_post_mha)
-        # self.logger.info(f"self.MHA.in_proj_weight.dtype = {self.MHA.in_proj_weight.dtype} | self.MHA.out_proj.weight.dtype = {self.MHA.out_proj.weight.dtype} | self.FFN.linear_projection.weight.dtype = {self.FFN.linear_projection.weight.dtype}")
-
         x_layer_norm_ffn = self.layer_norm_ffn(x_post_mha)
         x_post_ffn = x_post_mha + self.dropout_residual_ffn(self.FFN(x_layer_norm_ffn))
         return x_post_ffn
+        
+    # def forward(self, x):
+    #     batch_size, seq_len, d_model = x.shape
+    #     x_post_mha = self.layer_norm_mha(x + self.dropout_residual_mha(self.MHA(x=x)))
+    #     x_post_ffn = self.layer_norm_ffn(x_post_mha + self.dropout_residual_ffn(self.FFN(x_post_mha)))
+    #     return x_post_ffn
         
