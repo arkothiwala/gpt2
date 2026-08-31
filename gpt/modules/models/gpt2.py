ceb0665b4fd9ebfb7c1d15d6a9969dbb890e6f00
arkothiwala <ashutosh.kothiwala@gmail.com>
2026-08-31 06:57:23 +0000

    NOPE

--- gpt/modules/models/gpt2.py
+++ gpt/modules/models/gpt2.py
@@ -22,13 +22,13 @@ class GPT2Model(torch.nn.Module):
             max_norm=None, # changing max_norm to None -> will let model figure unless the training is unstable and we see exploding gradients.
             norm_type=2
         )
-        self.learnt_position_embedding = torch.nn.Embedding(
-            num_embeddings=self.context_length,
-            embedding_dim=self.d_model,
-            padding_idx=None,
-            max_norm=None, # changing max_norm to None -> will let model figure unless the training is unstable and we see exploding gradients.
-            norm_type=2
-        )
+        # self.learnt_position_embedding = torch.nn.Embedding(
+        #     num_embeddings=self.context_length,
+        #     embedding_dim=self.d_model,
+        #     padding_idx=None,
+        #     max_norm=None, # changing max_norm to None -> will let model figure unless the training is unstable and we see exploding gradients.
+        #     norm_type=2
+        # )
         # self.position_embedding = SinusoidalPositionalEmbeddings(d_model=self.d_model, max_seq_len=self.context_length)
         # self.embedding_layer_norm = TorchLayerNorm(normalized_shape=self.d_model)
         # self.transformer_layers = torch.nn.Sequential()
@@ -51,6 +51,7 @@ class GPT2Model(torch.nn.Module):
         # predict token with softmax
         # self.transformer_layers.append(torch.nn.Linear(in_features=self.d_model, out_features=self.vocab_size))
         self.dropout = torch.nn.Dropout(p=0.1)
+        # self.initialize_model_parameters(scaling_factor=1)
         self.initialize_model_parameters(scaling_factor=1/np.sqrt(2*self.n_layers))
         self.print_param_stats()
 
@@ -127,11 +128,11 @@ class GPT2Model(torch.nn.Module):
         x_learnt_embeddings = self.embedding(x)
         # self.logger.info(f"x_learnt_embeddings.var = {str(x_learnt_embeddings.var().item())} | x_learnt_embeddings.mean = {str(x_learnt_embeddings.mean().item())}")
         # self.logger.debug(f"x_learnt_embeddings.shape = {x_learnt_embeddings.shape} | x_learnt_embeddings.device = {x_learnt_embeddings.device} | x_learnt_embeddings.dtype = {x_learnt_embeddings.dtype}")
-        # x_pos_embeddings = self.position_embedding(x)
+        # x_pos_embeddings = self.position_embedding(x) # added scaling factor of 0.0008 so that the std dev of positional embeddings is 0.02 => which is set for the learnt embeddings in GPT2 architecture. Sinusoidal embeddings have variance of 0.5.
         # self.logger.info(f"x_pos_embeddings.var = {str(x_pos_embeddings.var().item())} | x_pos_embeddings.mean = {str(x_pos_embeddings.mean().item())}")
-        x_pos_embeddings = self.learnt_position_embedding(torch.arange(start=0, end=seq_len, device=x.device)).unsqueeze(0)
+        # x_pos_embeddings = self.learnt_position_embedding(torch.arange(start=0, end=seq_len, device=x.device)).unsqueeze(0)
         # self.logger.debug(f"x_pos_embeddings.shape = {x_pos_embeddings.shape} | x_pos_embeddings.device = {x_pos_embeddings.device} | x_pos_embeddings.dtype = {x_pos_embeddings.dtype}")
-        x_embeddings = x_learnt_embeddings + x_pos_embeddings
+        x_embeddings = x_learnt_embeddings #+ x_pos_embeddings
         # x_embeddings = self.embedding_layer_norm(x_embeddings)
         # self.logger.info(f"post layer norm - x_embeddings.var = {str(x_embeddings.var().item())} | x_embeddings.mean = {str(x_embeddings.mean().item())}")
         # x_embeddings = torch.nn.functional.dropout(input=x_embeddings, p=0.1) # MISTAKE - I had initially used functional dropout here w/o train v/s inference mode check. Moving it to Dropout module which internally manages train v/s inference mode and also makes code cleaner.
